a546eb8c6e441977d18406744d5eb297da7c465f
Sanchinn <gabrielheriqueps70@gmail.com>
2026-03-18 14:41:53 -0300

    add

---
+++ Aula 6/script.js
@@ -0,0 +1,15 @@
+document .getElementById("d1").innerHTML = "Prof. Fagner Pimentel";
+
+let nome = prompt("Digite seu nome: ");
+let idade = prompt("Digite sua idade: ");
+let ano = 2026;
+
+let nasc = ano-idade;
+
+document .getElementById("resp_ex1"). innerHTML = "Olá", nome + ", seu ano de nascimento é: ";
+nasc + "!"
+
+function mensagem(){
+    alert("Olá!")
+}
+
