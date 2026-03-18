488f3a7bddee71e9e0edec40b83bd9cde4de9c79
Sanchinn <gabrielheriqueps70@gmail.com>
2026-03-18 16:25:02 -0300

    adicionando

--- Aula 6/script.js
+++ Aula 6/script.js
@@ -13,8 +13,39 @@ function mensagem(){
     alert("Olá!")
 }
 
-mensagem{}
-mensagem{}
-mensagem{"Fagner"};
-mensagem{"Pimentel"};
+//mensagem{}
+//mensagem{}
+//mensagem{"Fagner"};
+//mensagem{"Pimentel"};
 
+function soma(a,b){return a + b;}
+function soma(a,b){return a - b;}
+function soma(a,b){return a * b;}
+function soma(a,b){return a / b;}
+
+resp = soma(5,56);
+document .getElementById("soma").innerHTML = resp 
+
+function exemplo2(){
+    let x = document .getElementById("in_ex2").value;
+
+    for(let i= 1; i < x; i++){
+        console.log(x + i);
+    }
+
+    document .getElementById("resp_ex2"). innerHTML = x;
+        
+}
+
+function exemplo3() {
+    let a = parseInt(document.getElementById("In_1_ex3").value);
+    let b = parseInt(document);
+}
+
+function exemplo4(){
+    let a = parseInt(document.getElementById("in_1_ex4").value)
+    let b = parseInt(document.getElementById("in_2_ex4").value)
+
+    let resp
+    
+}
