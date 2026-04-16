ff0262352e4885344f73632e7f2305f3d5ff8cd0
Sanchinn <gabrielhenriqueps70@gmail.com>
2026-04-16 15:26:04 -0300

    add

--- LAB 5/script.js
+++ LAB 5/script.js
@@ -33,7 +33,7 @@ ctx.closePath();
 ctx.beginPath();
 ctx.lineWidth = 2;
 ctx.fillStyle = "red";
-ctx.fillRect(180,250,60,60);
+ctx.fillRect(185,250,65,65);
 ctx.closePath();
 
 // Quadrado 6 (centro esquerda)
@@ -47,7 +47,7 @@ ctx.closePath();
 ctx.beginPath();
 ctx.lineWidth = 2;
 ctx.fillStyle = "cyan";
-ctx.fillRect(180,250,60,60);
+ctx.fillRect(460,215,40,70);
 ctx.closePath();
 
 // Texto
@@ -68,3 +68,50 @@ ctx.lineTo(500, 250);
 ctx.stroke();
 ctx.closePath();
 
+// Diagonal 1 (azul)
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.strokeStyle = 'blue';
+ctx.moveTo(0, 0);
+ctx.lineTo(500, 500);
+ctx.stroke();
+ctx.closePath();
+
+// Diagonal 2 (vermelha)
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.strokeStyle = 'red';
+ctx.moveTo(0, 500);
+ctx.lineTo(500, 0);
+ctx.stroke();
+ctx.closePath();
+
+// Arco Esquerdo
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'green';
+ctx.arc(120,380,25,0.5*Math.PI,2.5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+// Arco Direito
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'yellow';
+ctx.strokeStyle = 'green';
+ctx.arc(380,380,25,0.5*Math.PI,2.5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+// Arco Esquerdo
+ctx.beginPath();
+ctx.lineWidth = 2;
+ctx.fillStyle = 'cyan';
+ctx.strokeStyle = 'blue';
+ctx.arc(250,190,25,0.5*Math.PI,2.5*Math.PI);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
