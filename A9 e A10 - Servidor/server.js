e5aa59055084a1a7d073c84283e20c1b6eb65ba0
Sanchinn <gabrielhenriqueps70@gmail.com>
2026-05-06 15:17:35 -0300

    add

--- A9 e A10 - Servidor/server.js
+++ A9 e A10 - Servidor/server.js
@@ -2,6 +2,34 @@ require ("colors")
 var http = require("http")
 var express = require("express")
 var bodyParser = require("body-parser")
+var mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const url = "mongodb://gabrielhenriqueps70_db_user:<db_password>@ac-kw7hdei-shard-00-00.xfxhrct.mongodb.net:27017,ac-kw7hdei-shard-00-01.xfxhrct.mongodb.net:27017,ac-kw7hdei-shard-00-02.xfxhrct.mongodb.net:27017/?ssl=true&replicaSet=atlas-mofqlm-shard-0&authSource=admin&appName=Test";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+var dbo = client.db("exemplo_bd");
+var usuarios = dbo.collection("usuarios");
+
+
+app.post("/Cadastrar_usuario", function(req,res) {
+    
+    var data = {
+        db_nome: req.body.nome,
+        db_login: req.body.login,
+        db_senha: req.body.senha
+    }
+
+    usuarios.insertOne(data, function (err) {
+        if (err) {
+            response.render('resposta_usuario', {resposta: 'Erro ao cadastrar usuário!'})
+        }else {
+            response.render('resposta_usuario', {resposta: 'Usuário cadastrado com sucesso!'})
+        };
+    });
+
+});
+
 
 var app = express()
 app.use(express.static("./public"))
@@ -49,3 +77,4 @@ app.get("/for", function(req,res){
     var qtde = req.query.qtde;
     res.render("exemplo_for.ejs",{qtde})
 })
+
